803ed7b5f1993b85db614b9f8534767833f5df96
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2022-12-18 10:48:58 +0100

    Create Results Component and render it in searchParams

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 import useBreedList from "./useBreedList";
+import Results from "./Results";
 
 const SearchParams = () => {
     const [location, setLocation] = useState("")
@@ -69,9 +69,7 @@ const SearchParams = () => {
                 </label>
             </form>
             {
-                pets.map((pet) => (
-                    <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
-                ))
+                <Results pets={pets} />
             }
         </div>
     );
