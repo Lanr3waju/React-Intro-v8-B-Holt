1b4f7ea32f72c61275e1f21ff9462e81dc2674dc
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2022-12-18 12:30:40 +0100

    Instantiate ReactQuery in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import { createElement } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Details from './Details'
 import SearchParams from './SearchParams'
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+            cacheTime: Infinity
+        }
+
+    }
+})
 
 const App = () => {
     return (
         <BrowserRouter>
-            <header>
-                <Link to="/">Adopt Me!</Link>
-            </header>
-            <Routes>
-                <Route path="/details/:id" element={<Details />} />
-                <Route path="/" element={<SearchParams />} />
-            </Routes>
+            <QueryClientProvider client={queryClient}>
+                <header>
+                    <Link to="/">Adopt Me!</Link>
+                </header>
+                <Routes>
+                    <Route path="/details/:id" element={<Details />} />
+                    <Route path="/" element={<SearchParams />} />
+                </Routes>
+            </QueryClientProvider>
         </BrowserRouter>
     )
 }
