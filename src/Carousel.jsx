8c22fd77db82cb1c4e3ab4a560322655348d9a97
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2022-12-25 00:06:15 +0100

    Refactor Carousel from Class component to functional component

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,35 +1,30 @@
-import { Component } from "react";
+import { useState } from "react"
+function Carousel({ images }) {
+    const [active, setActive] = useState(0)
 
-class Carousel extends Component {
-    state = {
-        active: 0
+    const handleIndexClick = (e) => {
+        setActive(+e.target.dataset.index)
     }
 
-    static defaultProps = {
-        images: ["https://pets-images.dev-apis.com/pets/none.jpg"]
-    }
+    return (
+        <div className="carousel">
+            <img src={images[active] || "https://pets-images.dev-apis.com/pets/none.jpg"} alt="animal hero" />
+            <div className="carousel-smaller">
+                {images.map((photo, index) => (
+                    // eslint-disable-next-line
+                    <img
+                        onClick={handleIndexClick}
+                        data-index={index}
+                        key={photo}
+                        src={photo}
+                        className={index === active ? "active" : ""}
+                        alt="animal thumbnail"
+                    />
+                ))}
 
-    render() {
-        const { active } = this.state
-        const { images } = this.props
-
-        return (
-            <div className="carousel">
-                <img src={images[active]} alt="animal hero" />
-                <div className="carousel-smaller">
-                    {images.map((photo, index) => (
-                        <img
-                            key={photo}
-                            src={photo}
-                            className={index === active ? "active" : ""}
-                            alt="animal thumbnail"
-                        />
-                    ))}
-
-                </div>
-            </div >
-        )
-    }
+            </div>
+        </div >
+    )
 }
 
 export default Carousel
